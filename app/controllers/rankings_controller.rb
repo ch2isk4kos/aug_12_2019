d807e15dbba55b9200de17f7ea8bfebcd916bb3e
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-12 23:44:17 -0400

    added ranking crud actions

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,2 +1,49 @@
 class RankingsController < ApplicationController
+
+    before_action :find_ranking, only: [:show]
+
+    def index
+        @rankings = Ranking.all.order("created_at DESC")
+    end
+
+    def new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+
+            @ranking = @user.rankings.build # user
+            # @ranking.user_id = current_user.id
+
+            # category
+
+            5.times do # selections
+                selection_builder = @ranking.selections.build # .build == builds a has_many relationship
+                selection_builder.build_player # .build_object == builds a belongs_to relationship
+            end
+        else
+            @ranking = Ranking.new
+        end
+    end
+
+    def create
+        @ranking = current_user.rankings.build(ranking_params) # @ranking = Ranking.new(ranking_params)
+
+        if @ranking.save
+            redirect_to ranking_path(@ranking)
+        else
+            render :new
+        end
+
+    end
+
+    def show; end
+
+    private
+
+    def ranking_params
+        params.require(:ranking).permit(:description, player_ids: [], selections_attributes: [:player_id, player_attributes: [:name]])
+    end
+
+    def find_ranking
+        @ranking = Ranking.find(params[:id])
+    end
+
 end
