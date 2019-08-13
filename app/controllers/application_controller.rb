23c71582df0a0b5a1c2cc5df9867a5edf6963d62
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-12 22:17:08 -0400

    added session crud actions && included helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    include ApplicationHelper # points to && accesses data from application_helper.rb to be used throughout the entire program.
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+    def new; end
+
+    # login (create session)
+    def create
+        user = User.find_by(email: params[:user][:email])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            render :new
+        end
+    end
+
+    # logout (destroy session)
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+    
 end
