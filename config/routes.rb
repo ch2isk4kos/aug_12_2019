b7de29d70b4135ab0d8086c3b712fa3c6a5eed71
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-12 22:19:32 -0400

    add resources for login && logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
     post 'signup', to: 'users#create'
 
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+
+    delete 'logout', to: 'sessions#destroy'
+
     resources :players
     resources :rankings
     resources :users
