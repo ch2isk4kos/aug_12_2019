d3332ca811d8c6dbc3e0d9c589cae1284324b996
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-12 23:12:56 -0400

    added player crud actions

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,38 @@
 class PlayersController < ApplicationController
+
+    before_action :find_player, only: [:show]
+
+    def index
+        @players = Player.all
+    end
+
+    def new
+        @player = Player.new
+        # binding.pry
+    end
+
+    def create
+        @player = Player.new(player_params)
+
+        if @player.save
+            redirect_to player_path(@player)
+        else
+            render :new
+        end
+    end
+
+    def show
+        redirect_to players_path if !@player
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:name)
+    end
+
+    def find_player
+        @player = Player.find(params[:id])
+    end
+
 end
