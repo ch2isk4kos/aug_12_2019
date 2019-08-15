6e8a83008c343f1bb73dd002aaff96d206768de0
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-15 16:01:03 -0400

    added form_for ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -8,10 +8,7 @@ class RankingsController < ApplicationController
 
     def new
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
-
-            @ranking = @user.rankings.build # user
-            # @ranking.user_id = current_user.id
-
+            @ranking = @user.rankings.build # build user (has_many)
             # category
 
             5.times do # selections
@@ -20,12 +17,17 @@ class RankingsController < ApplicationController
             end
         else
             @ranking = Ranking.new
+            5.times do # selections
+                selection_builder = @ranking.selections.build # .build == builds a has_many relationship
+                selection_builder.build_player # .build_object == builds a belongs_to relationship
+            end
+
         end
     end
 
     def create
         @ranking = current_user.rankings.build(ranking_params) # @ranking = Ranking.new(ranking_params)
-
+        binding.pry
         if @ranking.save
             redirect_to ranking_path(@ranking)
         else
