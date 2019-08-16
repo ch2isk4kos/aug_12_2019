f4adb4d56fa700b14e944a4b42cd5dd02ef903ba
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-16 16:04:58 -0400

    re-seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Category.create(title: "All-Time")
+Category.create(title: "Point Guards")
+Category.create(title: "Shooting Guards")
+Category.create(title: "Small Fowards")
+Category.create(title: "Power Fowards")
+Category.create(title: "Centers")
+Category.create(title: "Dunkers")
+Category.create(title: "Shooters")
+Category.create(title: "Sixth Man")
+
 Player.create(name: "Michael Jordan")
 Player.create(name: "Patrick Ewing")
 Player.create(name: "Lebron James")
