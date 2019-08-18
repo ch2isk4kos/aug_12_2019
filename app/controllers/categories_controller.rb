ce933ad975e90e66aa851745b41e25d992a141b1
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-18 13:23:04 -0400

    added resources for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,21 @@
 class CategoriesController < ApplicationController
+
+    before_action :find_category, only: [:show]
+
+    def index
+        @categories = Category.all
+    end
+
+    def show
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:title)
+    end
+
+    def find_category
+        @category = Category.find(params[:id])
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_many :rankings
+    has_many :users, through: :rankings
 end
