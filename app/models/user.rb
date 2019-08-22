0906d619ca28ce5154d165e006d7c08b3b4b2957
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-22 15:50:15 -0400

    added comments to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+    # bcrypt encryption
     has_secure_password
+
+    # associations
     has_many :rankings
     has_many :categories, through: :rankings
 
+    # called in sessions#google_auth
     def self.from_omniauth(auth)
         where(email: auth.info.email).first_or_initialize do |user|
             user.username = auth.info.name
