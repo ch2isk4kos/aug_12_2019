1643db608fedd4902da37cb020eecf400173ea63
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-22 15:35:05 -0400

    added class method which is being called by sessions#google_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
     has_secure_password
     has_many :rankings
     has_many :categories, through: :rankings
+
+    def self.from_omniauth(auth)
+        where(email: auth.info.email).first_or_initialize do |user|
+            user.username = auth.info.name
+            user.email = auth.info.email
+            user.password = SecureRandom.hex
+        end
+    end
+
 end
