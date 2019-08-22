14b12691d390bd29dcf69d86ac1721764b3e9466
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-22 15:10:50 -0400

    added get request for google omniauth callback routed to the sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
 
+    get 'auth/:provider/callback', to: 'sessions#google_auth'
+
     delete 'logout', to: 'sessions#destroy'
 
     resources :categories
