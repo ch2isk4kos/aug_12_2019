2ba51b1d4d588f8f3a60db6e802d8cfb569dba4b
ch2isk4kos <ch2isk4kos@gmail.com>
2019-08-22 15:23:49 -0400

    added google_auth action to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,10 +14,23 @@ class SessionsController < ApplicationController
         end
     end
 
+    def google_auth
+        user = User.from_omniauth(auth)
+        user.save
+        session[:user_id] = user.id
+        redirect_to ranking_path(user)
+    end
+
     # logout (destroy session)
     def destroy
         session.clear
         redirect_to root_path
     end
-    
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
+    end
+
 end
